4c595ac5abc6948a0f58b84ea81a6cbde5ad8d85
thenry3 <43651306+thenry3@users.noreply.github.com>
2019-05-08 19:57:49 -0700

    Implemented cards into Game.java

--- app/src/main/java/com/example/tongits/Deck.java
+++ app/src/main/java/com/example/tongits/Deck.java
@@ -7,39 +7,52 @@ public class Deck {
     //CONSTRUCTOR
     public Deck()
     {
-        card_deck = new Card[52];
-        numberOfCardsInDeck = 0;
+        card_deck = new ArrayList<>();
 
         // make all the cards
         for (int suit = 1; suit < 5; suit++)
         {
             for (int value = 1; value < 14; value++)
             {
-                card_deck[numberOfCardsInDeck] = new Card(suit, value);
-                numberOfCardsInDeck++;
+                card_deck.add(new Card(suit, value));
             }
         }
     }
 
+    // shuffle function
     public void shuffleDeck()
     {
         Random random = new Random();
 
         //For every card location, choose a random int and switch
-        for (int i = 0; i < numberOfCardsInDeck; i++) {
-            Card tempcard = card_deck[i];
-            card_deck[i] = card_deck[random.nextInt() % 53];
-            card_deck[i] = tempcard;
+        for (int i = 0; i < getNumberOfCardsInDeck(); i++) {
+            int randomInt = random.nextInt() % 53;
+            Card tempcard = card_deck.get(i);
+            card_deck.set(i, card_deck.get(randomInt));
+            card_deck.set(randomInt, tempcard);
         }
     }
 
     public int getNumberOfCardsInDeck()
     {
-        return numberOfCardsInDeck;
+        return card_deck.size();
+    }
+
+    public ArrayList<Card> getCardDeck(){
+        return card_deck;
+    }
+
+    public Card topCard(){
+        return card_deck.get(card_deck.size() - 1);
+    }
+
+    public void removeCard(){
+        if (getNumberOfCardsInDeck() > 0) {
+            card_deck.remove(getNumberOfCardsInDeck() - 1);
+        }
     }
 
 
     // PRIVATE VARIABLES
-    private Card[] card_deck;
-    private int numberOfCardsInDeck;
+    private ArrayList<Card> card_deck;
 }
--- app/src/main/java/com/example/tongits/Game.java
+++ app/src/main/java/com/example/tongits/Game.java
@@ -2,12 +2,55 @@ package com.example.tongits;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import java.util.*;
+
 
 public class Game extends AppCompatActivity {
+
+    ImageView mainDeckView;
+    ArrayList<Card> playerHand;
+    boolean playerTurn = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        mainDeckView = findViewById(R.id.cardDeck);
+
+        final Deck deckOfCards = new Deck();
+
+        //shuffle a few times for good measure
+        for (int i = 0; i < 5; i++){
+            deckOfCards.shuffleDeck();
+        }
+
+        playerHand = new ArrayList<>();
+        dealCards(deckOfCards, playerHand);
+
+        mainDeckView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (playerTurn)
+                {
+                    playerHand.add(deckOfCards.topCard());
+                    deckOfCards.removeCard();
+                    playerTurn = false;
+                }
+            }
+        });
+
+
+
+    }
+
+    void dealCards(Deck deck, ArrayList<Card> playerDeck){
+        for (int i = 0; i < 13; i++) {
+            playerDeck.add(deck.topCard());
+            deck.removeCard();
+        }
     }
 }
