34c772aca00611f635328595de12ef3dfc79b427
ashwin-s-ranade <ashwinranade99@gmail.com>
2019-06-02 12:40:58 -0700

    ashwin is cool and he made some buttons work well

--- app/src/main/java/com/example/tongits/Game.java
+++ app/src/main/java/com/example/tongits/Game.java
@@ -1,5 +1,6 @@
 package com.example.tongits;
 
+import android.content.Context;
 import android.support.annotation.DrawableRes;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +21,11 @@ public class Game extends AppCompatActivity {
 
     ImageView mainDeckView; //the icon for the main deck
     LinearLayout viewHand; //holds player's cards
+    LinearLayout AI1DiscardPilePage;
+
+    LinearLayout AI2DiscardPilePage;
+    LinearLayout PlayerDiscardPilePage;
+
     ImageButton AI1DiscardView;
     ImageButton playerDiscardView;
     ImageButton AI2DiscardView;
@@ -35,6 +41,13 @@ public class Game extends AppCompatActivity {
     ArrayList<Card> AI2Hand;
     boolean playerDrawTurn = true; //keeps track of player's turn
 
+    static boolean activeAI1 = false; //is the AI1DiscardPilePage Linear Layout open or not?
+    static boolean activeAI2 = false; //same for AI2
+    static boolean activePlayer = false; //same, but for player
+    static int tracker1 = 0; //trying to see if the same button is pressed twice
+    static int tracker2 = 0;
+    static int playertracker = 0;
+
     /*------------------------------------------------------------------*/
 
 
@@ -47,9 +60,16 @@ public class Game extends AppCompatActivity {
         mainDeckView = findViewById(R.id.cardDeck);
         viewHand = findViewById(R.id.playerHand);
         viewHand.getLayoutParams().width = 100;
+
+        AI1DiscardPilePage = findViewById(R.id.AI1DiscardPilePage); //this line matters! links to the name of the linear layout in the XML file
+
+
         AI1DiscardView = findViewById(R.id.AI1DiscardPile);
         AI2DiscardView = findViewById(R.id.AI2DiscardPile);
         playerDiscardView = findViewById(R.id.playerDiscardPile);
+
+        //AI1DiscardView.getLayoutParams().width = 100; this line is unnecessary? idk what it does
+
 
         final Deck deckOfCards = new Deck();
 
@@ -90,29 +110,70 @@ public class Game extends AppCompatActivity {
                 }
             });
 
-        AI1DiscardView.setOnClickListener(new View.OnClickListener() {
+
+
+        AI1DiscardView.setOnClickListener(new View.OnClickListener() { //this is for the first AI view
             @Override
             public void onClick(View v) {
+                updateAI1DiscardPile(1);
+                activeAI1 = !activeAI1;
 
+                if (activeAI1 == false && activeAI2 == false && activePlayer == false) {
+                    AI1DiscardPilePage.setVisibility(View.GONE);
+                    tracker1=tracker2=playertracker=0;
+                }
+                else{
+                    AI1DiscardPilePage.setVisibility(View.VISIBLE);
+
+                tracker1++;
+                tracker2=playertracker=0;
+                if (tracker1>=2) { //this line jank asf
+                    AI1DiscardPilePage.setVisibility(View.GONE);
+                    tracker1 =0;
+                }}
             }
         });
 
-        AI2DiscardView.setOnClickListener(new View.OnClickListener() {
+
+        AI2DiscardView.setOnClickListener(new View.OnClickListener() { //this is for the first AI view
             @Override
             public void onClick(View v) {
+                updateAI1DiscardPile(2);
+                activeAI2 = !activeAI2;
 
+                if (activeAI1 == false && activeAI2 == false && activePlayer == false) {
+                    AI1DiscardPilePage.setVisibility(View.GONE);
+                    tracker1=tracker2=playertracker=0;
+                }
+                else{
+                    AI1DiscardPilePage.setVisibility(View.VISIBLE);
+
+                tracker2++;
+                tracker1=playertracker=0;
+                if (tracker2>=2) { //this line jank asf
+                    AI1DiscardPilePage.setVisibility(View.GONE);
+                    tracker2 =0;
+                }}
             }
         });
 
-        playerDiscardView.setOnClickListener(new View.OnClickListener() {
+        playerDiscardView.setOnClickListener(new View.OnClickListener() { //this is for the first AI view
             @Override
             public void onClick(View v) {
-
+                updateAI1DiscardPile(3);
+                activePlayer = !activePlayer;
+                if (activeAI1 == false && activeAI2 == false && activePlayer == false) {
+                    AI1DiscardPilePage.setVisibility(View.GONE);
+                    tracker1=tracker2=playertracker=0;
+                }
+                else
+                    AI1DiscardPilePage.setVisibility(View.VISIBLE);
             }
         });
 
 
     }
+
 
     void dealCards(Deck deck, ArrayList<Card> playerDeck, int numCards){
         for (int i = 0; i < numCards; i++) {
@@ -177,7 +238,46 @@ public class Game extends AppCompatActivity {
             viewHand.addView(card);
         }
     }
+    void updateAI1DiscardPile(int ai) { //1 = ai1, 2 = ai2, 3 = player
+        AI1DiscardPilePage.removeAllViews();
+        if (ai == 1) {
+            for (int i = 0; i < AI1Discard.size(); i++) {
+                ImageButton card = new ImageButton(this);
+                setNewCardImage(AI1Discard.get(i).getSuit(), AI1Discard.get(i).getValue(), card);
+                AI1DiscardPilePage.addView(card);
+            }
+        } else if (ai == 2) {
+            for (int i = 0; i < AI2Discard.size(); i++) {
+                ImageButton card = new ImageButton(this);
+                setNewCardImage(AI2Discard.get(i).getSuit(), AI2Discard.get(i).getValue(), card);
+                AI1DiscardPilePage.addView(card);
+            }
+        }
+        else if (ai == 3){
+            for (int i = 0; i < playerDiscard.size(); i++) {
+                ImageButton card = new ImageButton(this);
+                setNewCardImage(playerDiscard.get(i).getSuit(), playerDiscard.get(i).getValue(), card);
+                AI1DiscardPilePage.addView(card);
+            }
+        }
+    }
 
+   /* void updateAI2DiscardPile(){
+        AI2DiscardPilePage.removeAllViews();
+        for (int i = 0; i < AI2Discard.size(); i++) {
+            ImageButton card = new ImageButton(this);
+            setNewCardImage(AI2Discard.get(i).getSuit(), AI2Discard.get(i).getValue(), card);
+            AI2DiscardPilePage.addView(card);
+        }
+    }
+    void updatePlayerDiscardPile(){
+        PlayerDiscardPilePage.removeAllViews();
+        for (int i = 0; i < playerDiscard.size(); i++) {
+            ImageButton card = new ImageButton(this);
+            setNewCardImage(playerDiscard.get(i).getSuit(), playerDiscard.get(i).getValue(), card);
+            PlayerDiscardPilePage.addView(card);
+        }
+    }*/
     // Depending on the suit and card, update the card's photo
     void setNewCardImage(int suit, int value, View card)
     {
