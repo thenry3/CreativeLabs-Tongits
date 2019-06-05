ec4933e33d925170e69f3c5cbc07d9d99d01878a
NilaElumalai <nilanandhie@gmail.com>
2019-06-05 15:08:33 -0700

    Fixed Last Instruction Panel

--- app/src/main/java/com/example/tongits/fragScreen5.java
+++ app/src/main/java/com/example/tongits/fragScreen5.java
@@ -2,13 +2,16 @@ package com.example.tongits;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class fragScreen5 extends Fragment {
 
+    private TextView description;
 
     public fragScreen5() {
         // Required empty public constructor
@@ -17,6 +20,10 @@ public class fragScreen5 extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_screen5, container, false);
+
+        description = view.findViewById(R.id.formatDesc3);
+        description.setText(Html.fromHtml(getString(R.string.desc3)));
+
         return view;
     }
 }
